fa3e187d5fc6c5402a9bbb51b421a44851a0dba9
bqlinh <14121611ll@gmail.com>
2023-01-04 17:52:03 +0700

    update

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent {
-
+  @Input() hero?: Hero;
 }
