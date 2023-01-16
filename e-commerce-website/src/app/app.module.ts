8d5dde821af9473cccf90208af55477c58272be5
bqlinh <14121611ll@gmail.com>
2023-01-16 09:03:29 +0700

    update

---
+++ e-commerce-website/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
+import { PeopleComponent } from './people/people.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ProductsComponent,
+    PeopleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ routing-app/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ChildAComponent } from './first/child-a/child-a.component';
+import { ChildBComponent } from './first/child-b/child-b.component';
+
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+const routes: Routes = [
+  { path: 'first-component', 
+    component: FirstComponent, 
+    children: [
+      { 
+        path: 'child-a',
+        component: ChildAComponent
+      }, 
+      {
+        path: 'child-b',
+        component: ChildBComponent
+      }
+    ]
+  },
+  { path: 'second-component', component: SecondComponent },
+  { path: '',   redirectTo: '/first-component', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
